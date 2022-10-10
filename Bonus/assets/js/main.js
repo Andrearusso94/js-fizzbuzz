112a8483f2a982f0bebc4f7d764943dfc2b770a7
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-10 16:10:49 +0200

    add bonus

---
+++ Bonus/assets/js/main.js
@@ -0,0 +1,94 @@
+/*Scrivi un programma che stampi in console i numeri da 1 a 100
+
+ma che per i multipli di 3 stampi “Fizz” al posto del numero
+
+e per i multipli di 5 stampi “Buzz”.
+
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.*/
+
+
+/*for (let i = 1; i < 101; i++){
+
+
+     if (i % 15 === 0) {
+        console.log("fizzbuzz")
+     }else if (i % 3 === 0){
+        console.log("fizz")
+    } else if (i % 5 === 0){
+        console.log("buzz")
+    }  else {
+        console.log(i)
+    }
+}*/
+           
+//BONUS 1:
+//Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+/*const listEl = document.querySelector("ul")
+
+
+  
+
+ for (let i = 1; i < 101; i++){
+     
+   if (i % 15 === 0) {
+    const li =document.createElement("li");
+    li.append("fizzbuzz");
+    listEl.append(li);
+    console.log("fizzbuzz")
+        
+     }else if (i % 3 === 0){
+        const li =document.createElement("li");
+        li.append("fizz");
+        listEl.append(li);
+        console.log("fizz")
+    } else if (i % 5 === 0){
+        const li =document.createElement("li");
+        li.append("buzz");
+        listEl.append(li);
+        console.log("buzz")
+    }  else {
+        const li =document.createElement("li");
+        li.append(`${i}`);
+        listEl.append(li);
+        console.log(i)
+    }
+ }*/
+
+ //BONUS 2
+
+ //Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. 
+    
+    
+
+ const listEl = document.querySelector("ul")
+
+
+  
+
+ for (let i = 1; i < 101; i++){
+     
+   if (i % 15 === 0) {
+    const li =document.createElement("li");
+    li.append("fizzbuzz");
+    listEl.append(li);
+    
+    console.log("fizzbuzz")
+        
+     }else if (i % 3 === 0){
+        const li =document.createElement("li");
+        li.append("fizz");
+        listEl.append(li);
+        console.log("fizz")
+    } else if (i % 5 === 0){
+        const li =document.createElement("li");
+        li.append("buzz");
+        listEl.append(li);
+        console.log("buzz")
+    }  else {
+        const li =document.createElement("li");
+        li.append(`${i}`);
+        listEl.append(li);
+        console.log(i)
+    }
+ }
