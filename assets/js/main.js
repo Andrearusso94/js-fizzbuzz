301f86533444753a0123d294d9497729978513e5
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-10 15:03:14 +0200

    add fizz-buzz-fizzbuzz

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,21 @@ e per i multipli di 5 stampi “Buzz”.
 
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.*/
 
+
 for (let i = 1; i < 101; i++){
-    console.log(i)
+
+     if (i % 15 === 0) {
+        console.log("fizzbuzz")
+     }else if (i % 3 === 0){
+        console.log("fizz")
+    } else if (i % 5 === 0){
+        console.log("buzz")
+    }  else {
+        console.log(i)
+    }
 }
+           
+      
+    
+    
+
