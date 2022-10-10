856425f922fa2305cd6e9066b6c65a66f3c81003
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-10 15:16:35 +0200

    add for tiket

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,8 @@ e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.*/
 
 
-for (let i = 1; i < 101; i++){
+/*for (let i = 1; i < 101; i++){
+
 
      if (i % 15 === 0) {
         console.log("fizzbuzz")
@@ -18,9 +19,38 @@ for (let i = 1; i < 101; i++){
     }  else {
         console.log(i)
     }
-}
+}*/
            
-      
+//BONUS 1:
+//Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+const listEl = document.querySelector("ul")
+
+
+  
+
+ for (let i = 1; i < 101; i++){
+     
+   const li =document.createElement("li");
+   li.append(i);
+   listEl.append(li);
+
+   if (i % 15 === 0) {
+    li.append(i);
+   listEl.append(li);
+        console.log("fizzbuzz")
+     }else if (i % 3 === 0){
+        li.append(i);
+   listEl.append(li);
+        console.log("fizz")
+    } else if (i % 5 === 0){
+        li.append(i);
+   listEl.append(li);
+        console.log("buzz")
+    }  else {
+        console.log(i)
+    }
+ }
     
     
 
